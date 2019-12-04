405640866f760a5350148b0fecb65f174f6c1c6b
Nicolas Pépin-Perreault <nicolas.pepin-perreault@camunda.com>
2019-12-04 11:41:54 +0100

    Revert "chore(ci): fix artifact signing on release"
    This reverts commit 3ab70e85e883abc2bcab726607396b4259848e1b.

--- .ci/scripts/release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mvn -s settings.xml release:prepare release:perform -B \
-    -Dgpg.passphrase="${GPG_PASS}" \
-    -Dresume=false \
-    -Dtag=${RELEASE_VERSION} \
-    -DreleaseVersion=${RELEASE_VERSION} \
-    -DdevelopmentVersion=${DEVELOPMENT_VERSION} \
-    -Darguments='--settings=settings.xml -DskipTests=true -Dgpg.passphrase="${GPG_PASS}"'
